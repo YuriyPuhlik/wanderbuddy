fc17295d20dd4f701b4b0c2344caa1b54d647fde
YuriyPuhlik <43183030+YuriyPuhlik@users.noreply.github.com>
2024-07-10 12:12:07 +0200

    Added Travelers People screen

---
+++ force-app/main/default/lwc/travelersPeople/travelersPeople.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+import person1Image from '@salesforce/resourceUrl/Person1';
+import person2Image from '@salesforce/resourceUrl/Person2';
+import person3Image from '@salesforce/resourceUrl/Person3';
+
+export default class TravelersPeople extends LightningElement {
+    person1ImageUrl = person1Image;
+    person2ImageUrl = person2Image;
+    person3ImageUrl = person3Image;
+}
